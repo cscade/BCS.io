4b75dd6683e0e90945b5ff7b1e7628cdc740b34c
Carson Christian <cc@amplego.com>
2012-10-29 13:02:36 -0400

    switch to express, socket.io

--- app.js
+++ app.js
@@ -6,11 +6,19 @@
 //  Copyright 2012 (ampl)EGO. All rights reserved.
 // 
 
-var connect = require('connect'),
-	app;
+var express = require('express'),
+	app = express(),
+	path = require('path'),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
 
-app = connect();
+app.use(express.static(path.join(__dirname, 'public')));
 
-app
-	.use(connect.static('public'))
-	.listen(8080);
+server.listen(8080);
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+});
